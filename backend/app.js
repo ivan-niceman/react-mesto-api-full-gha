bda055405b8a603e411e00b2b0b657fecfd91607
Ivan Niceman <sn.iv@bk.ru>
2023-06-20 18:37:26 +0700

    add crash-test

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,12 @@ app.use(express.json());
 
 app.use(limiter);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 app.use(errors());
 app.use(serverError);
