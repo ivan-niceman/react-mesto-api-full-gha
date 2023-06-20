059aecc3cb6a0175d469108fdd9ab0321cf375d5
Ivan Niceman <sn.iv@bk.ru>
2023-06-20 18:29:34 +0700

    настроил сервер, добавил cors

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,17 @@ const helmet = require('helmet');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 const router = require('./routes');
 const serverError = require('./errors/servererror');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+// mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
