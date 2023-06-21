2af6a21b7dc950d97034b470deb0775a93bb0763
Ivan Niceman <sn.iv@bk.ru>
2023-06-21 09:09:42 +0700

    добавил logger

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const router = require('./routes');
 const serverError = require('./errors/servererror');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -18,6 +19,30 @@ app.use(cors({
   ],
   credentials: true,
 }));
+
+const allowedCors = [
+  'https://nice-man.nomoredomains.rocks',
+  'http://nice-man.nomoredomains.rocks',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
+  }
+
+  next();
+});
 
 // mongoose.connect('mongodb://localhost:27017/mestodb');
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
@@ -34,6 +59,8 @@ app.use(express.json());
 
 app.use(limiter);
 
+app.use(requestLogger);
+
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
@@ -41,6 +68,9 @@ app.get('/crash-test', () => {
 });
 
 app.use(router);
+
+app.use(errorLogger);
+
 app.use(errors());
 app.use(serverError);
 
