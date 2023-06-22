54d5110596bf9c0c33f0e48a90c23d43a7ee8479
Ivan Niceman <sn.iv@bk.ru>
2023-06-22 15:46:58 +0700

    поменял api

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "https://auth.nomoreparties.co";
-export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "https://api.nice-man.nomoredomain.nomoredomains.work";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.nice-man.nomoredomain.nomoredomains.work";
 
 const makeRequest = (url, method, body, token) => {
   const options = {
